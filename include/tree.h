02a8e97fefe81faab90a54e22eb552ce51d1cb73
EvseevDA <99210872+EvseevDA@users.noreply.github.com>
2022-06-05 23:05:06 +0300

    Update tree.h

--- include/tree.h
+++ include/tree.h
@@ -1,5 +1,23 @@
 // Copyright 2022 NNTU-CS
 #ifndef INCLUDE_TREE_H_
 #define INCLUDE_TREE_H_
+#include <vector>
+#include <string>
+
+class Tree {
+ private:
+    std::vector<std::string> permutations;
+    struct Node;
+    Node* root;
+    
+    void perm(Node*, std::string);
+    
+    void makeTree(Node*, std::vector<char>);
+    
+ public:
+    explicit Tree(const std::vector<char> &);
+    
+    std::string operator [] (const unsigned int i) const;
+};
 
 #endif  // INCLUDE_TREE_H_
