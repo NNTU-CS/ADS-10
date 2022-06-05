f795d9bc1003d70cdc357bb047e9525f9963f078
PRIGARINA-VICTORIA <99180874+PRIGARINA-VICTORIA@users.noreply.github.com>
2022-06-05 22:24:55 +0300

    Update tree.h

--- include/tree.h
+++ include/tree.h
@@ -5,47 +5,48 @@
 #include <iostream>
 #include <string>
 class Tree {
-private:
+ private:
 	struct  Branch {
 		char Data;
 		std::vector<Branch*> subsidiary;
 	};
 	Branch* root;
-
+	
 	std::vector <char> val;
 	std::string element;
 	std::vector<char> trunsf;
 	std::vector <std::vector<char>> res;
-
-public:
-	explicit Branch(std::vector <char> a) {
-		Data = a;
+	
+ public:
+	
+	explicit Branch(std::vector <char> in) {
+		Data = in;
 		element.resize(a.size());
 		transf.resize(a.size());
 		Branch* root = new Branch;
 		root->Data = '*';
-		transform(a, -1, 0, root);
+		transformm(in, -1, 0, root);
 	}
-
+	
 	Branch* Create_Tree(char val) {
 		Branch* temp = new Branch;
 		temp->Data = val;
 		return temp;
 	}
-	void Transform(std::vector <char> a, int b, int num, Branch* root) {
-		if (a.size() == 1) {
+	void Transform(std::vector <char> in, int b, int num, Branch* root) {
+		if (in.size() == 1) {
 			res.push_back(tras);
 			return;
 		}
-		auto c = a.cbegin();
-		if (d >= 0) {
+		auto c = in.cbegin();
+		if (b >= 0) {
 			num++;
-			a.erase(c + d);
+			in.erase(c + b);
 		}
-		for (int i = 0; i < a.size(); i++) {
+		for (int i = 0; i < in.size(); i++) {
 			root->subsidiary.push_back(Create_Tree(a[i]));
-			transf[num] = a[i];
-			transform(a, i, num, root->subsidiary[i]);
+			transf[num] = in[i];
+			transformm(in, i, num, root->subsidiary[i]);
 		}
 	}
 	std::vector<char> getPerm(Tree tree, int num) {
