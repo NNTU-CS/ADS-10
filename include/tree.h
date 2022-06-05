6bd180c4816d063e4995e871ca961c559885fe33
EvseevDA <99210872+EvseevDA@users.noreply.github.com>
2022-06-05 23:13:55 +0300

    Update tree.h

--- include/tree.h
+++ include/tree.h
@@ -9,15 +9,15 @@ class Tree {
     std::vector<std::string> permutations;
     struct Node;
     Node* root;
-    
+
     void perm(Node*, std::string);
-    
+
     void makeTree(Node*, std::vector<char>);
-    
+
  public:
     explicit Tree(const std::vector<char> &);
-    
-    std::string operator [] (const unsigned int i) const;
+
+    std::string operator[] (const unsigned int i) const;
 };
 
 #endif  // INCLUDE_TREE_H_
