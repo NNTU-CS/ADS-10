62e8df0580523da8b3eed0a3fe69dea38618aa57
ShuvalovaEA <eshalos1207@gmail.com>
2024-05-23 23:00:15 +0300

    222ads10

--- include/tree.h
+++ include/tree.h
@@ -2,4 +2,60 @@
 #ifndef INCLUDE_TREE_H_
 #define INCLUDE_TREE_H_
 
+#include <vector>
+
+class Tree {
+private:
+    struct Node {
+        std::vector<Node*> children;
+        char value;
+    };
+    Node* root;
+    std::vector<std::vector<char>> permutations;
+
+    void buildTree(Node* node, std::vector<char> values) {
+        if (!node) {
+            node = new Node;
+        }
+        if (values.empty()) {
+            return;
+        }
+        for (int i = 0; i < values.size(); i++) {
+            std::vector<char> remainingValues = values;
+            Node* child = new Node;
+            child->value = values[i];
+            node->children.push_back(child);
+            remainingValues.erase(remainingValues.begin() + i);
+            buildTree(node->children.back(), remainingValues);
+        }
+    }
+
+    void generatePermutations(Node* node, std::vector<char>& current) {
+        for (int i = 0; i < node->children.size(); i++) {
+            current.push_back(node->children[i]->value);
+            if (node->children[i]->children.empty()) {
+                if (current.size() != 1) {
+                    permutations.push_back(current);
+                }
+            }
+            generatePermutations(node->children[i], current);
+            current.pop_back();
+        }
+    }
+
+public:
+    explicit Tree(std::vector<char> values): root(nullptr) {
+        root = new Node;
+        buildTree(root, values);
+        std::vector<char> current;
+        generatePermutations(root, current);
+    }
+
+    std::vector<char> getPermutation(int index) const {
+        if (index < 0 || index >= permutations.size()) {
+            return std::vector<char>();
+        }
+        return permutations[index];
+    }
+};
 #endif  // INCLUDE_TREE_H_
