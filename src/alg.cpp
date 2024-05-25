720a28902bd9a82f7dedded642f609cd0a35e778
Tsvetov1984 <acvetov68@gmail.com>
2024-05-25 16:01:02 +0300

    ASD-10

--- src/alg.cpp
+++ src/alg.cpp
@@ -2,9 +2,17 @@
 #include  <iostream>
 #include  <fstream>
 #include  <locale>
+#include <vector>
 #include  <cstdlib>
 #include  "tree.h"
 
-std::vector<char> getPerm(const Tree& tree, int n) {
-  // напишите реализацию
+std::vector<char> getPermutationByIndex(const PermutationTree &tree, int index) {
+    std::vector<std::vector<char>> permutations = tree.getAllPermutations();
+    size_t size = permutations.size();
+    std::vector<char> result;
+
+    while (index > 0) {
+        result = permutations[index - 1];
+    }
+    return result;
 }
