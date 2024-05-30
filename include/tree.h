972db0ec2552da05e681a9eb9ffab87f72fd6c6d
Анастасия <98629650+Veniaminova@users.noreply.github.com>
2024-05-30 13:30:22 +0300

    tree.h

--- include/tree.h
+++ include/tree.h
@@ -1,5 +1,48 @@
 // Copyright 2022 NNTU-CS
 #ifndef INCLUDE_TREE_H_
 #define INCLUDE_TREE_H_
+#include <vector>
+#include <iostream>
+
+using namespace std;
+
+class Node {
+public:
+    int value;
+    std::vector<Node*> ch;
+
+    Node(int value) : value(value) {}
+};
+
+class Tree {
+ private:
+    Node* root;
+
+    Tree(const std::vector<char>& values) {
+        root = buildTree(values);
+    }
+
+ public:
+    Node* buildTree(const std::vector<char>& values) {
+        if (values.empty()) {
+            return nullptr;
+        }
+
+        std::vector<std::vector<Node*>> levels;
+        levels.push_back({ new Node(values[0]) });
+
+        for (int i = 1; i < values.size(); i++) {
+            std::vector<Node*> currentLevel;
+            for (int j = 0; j < levels[levels.size() - 1].size(); j++) {
+                Node* node = levels[levels.size() - 1][j];
+                node->ch.push_back(new Node(values[i]));
+                currentLevel.push_back(node->ch.back());
+            }
+            levels.push_back(currentLevel);
+        }
+
+        return levels[0][0];
+    }
+};
 
 #endif  // INCLUDE_TREE_H_
