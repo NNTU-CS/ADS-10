7c9c828338a0afc663dafa72f557f61de6ddfd59
Анастасия <98629650+Veniaminova@users.noreply.github.com>
2024-05-30 23:28:41 +0300

    alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -6,26 +6,17 @@
 #include  <vector>
 #include  "tree.h"
 
-void getPermHelper(TreeNode* node, int index, const std::vector<char>& result) {
-    if (node == nullptr) {
-        return;
-    }
-
-    if (index == 0) {
-        result.insert(result.begin(), node->value);
-        return;
-    }
-
-    --index;
-    for (TreeNode* child : node->children) {
-        getPermHelper(child, index, result);
-        if (index == 0) {
-            break;
-        }
-    }
-}
-std::vector<char> getPerm(Tree* tree, int index) {
+std::vector<char> getPerm(const Tree& tree, int index) {
+    Node* current = tree.root;
     std::vector<char> result;
-    getPermHelper(tree->root, index, result);
-    return result;
+
+    for (int i = 0; i < tree.root->children.size(); i++) {
+        if (i == index) {
+            result.push_back(current->children[i]->value);
+            return result;
+        }
+        current = current->children[i];
+    }
+
+    return {};
 }
