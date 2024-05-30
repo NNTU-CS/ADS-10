363a82f5c34edd4a25142f18da6dfcb53bcbf15d
Анастасия <98629650+Veniaminova@users.noreply.github.com>
2024-05-30 23:28:10 +0300

    tree.h

--- include/tree.h
+++ include/tree.h
@@ -5,29 +5,95 @@
 #include <string>
 #include <vector>
 
-class TreeNode {
+class Node {
  public:
+    std::vector<Node*> children;
     char value;
-    std::vector<TreeNode*> children;
+    int count;
 
-    explicit TreeNode(char value) : value(value) {}
-};
+    explicit Node(char val) : value(val), count(1) {}
 
-class Tree {
- public:
-    TreeNode* root;
-
-    explicit Tree(const std::string& symbols) {
-        root = new TreeNode(symbols[0]);
-        for (size_t i = 1; i < symbols.size(); ++i) {
-            addChild(root, symbols[i]);
+    ~Node() {
+        for (auto child : children) {
+            delete child;
         }
     }
 
-    void addChild(TreeNode* node, char value) {
-        TreeNode* newNode = new TreeNode(value);
-        node->children.push_back(newNode);
+    Node* findChild(char val) {
+        for (Node* child : children) {
+            if (child->value == val) {
+                return child;
+            }
+        }
+        return nullptr;
+    }
+
+    void incrementCount() {
+        count++;
     }
 };
+class Tree {
+ public:
+    Node* root;
+
+    explicit Tree(const std::vector<char>& elements) {
+        root = new Node('\0');
+        buildTree(root, elements);
+    }
+
+    ~Tree() {
+        delete root;
+    }
+
+    std::vector<std::string> getAllPermutations() const {
+        std::vector<std::string> permutations;
+        genPerm(root, "", &permutations);
+        return permutations;
+    }
+
+    std::string getPrime(int index) const {
+        std::string result;
+        genPrimeHelper(root, index, result);
+        return result;
+    }
+
+ private:
+    void buildTree(Node* node, const std::vector<char>& elements) {
+        for (char element : elements) {
+            Node* child = new Node(element);
+            node->children.push_back(child);
+            std::vector<char> el = elements;
+            el.erase(std::remove(el.begin(), el.end(), element), el.end());
+            buildTree(child, el);
+        }
+    }
+
+    void genPerm(Node* n, std::string cur, std::vector<std::string>* p) const {
+        if (n->children.empty()) {
+            p->push_back(cur);
+            return;
+        }
+        for (auto child : n->children) {
+            genPerm(child, cur + n->value, p);
+        }
+    }
+
+    void genPrimeHelper(Node* n, int& index, std::string& result) const {
+        if (n->children.empty()) {
+            if (index == 0) {
+                result = n->value;
+            }
+            else {
+                --index;
+            }
+            return;
+        }
+        for (auto child : n->children) {
+            genPrimeHelper(child, index, result);
+        }
+    }
+};
+
+std::vector<char> getPerm(const Tree& tree, int index);
 
 #endif  // INCLUDE_TREE_H_
