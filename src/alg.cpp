c0eb60e377e7773cb02ac640fdd2ff3bddf9f6f8
Funibi <139475082+Funibi@users.noreply.github.com>
2024-05-31 13:10:16 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -7,19 +7,19 @@
 
 void getPermutation(TreeNode* node, int* n, std::vector<char>* result, 
                     std::vector<char>* current) {
-    if (node->children.empty()) {
-        if (n == 0) {
-            result = current;
+ if (node->children.empty()) {
+        if (*n == 0) {
+            *result = *current;
         }
-        --n;
+        --(*n);
         return;
     }
 
     for (TreeNode* child : node->children) {
-        current.push_back(child->value);
+        current->push_back(child->value);
         getPermutation(child, n, result, current);
-        current.pop_back();
-        if (!result.empty()) return;
+        current->pop_back();
+        if (!result->empty()) return;
     }
 }
 
@@ -27,6 +27,6 @@ std::vector<char> getPerm(const Tree& tree, int n) {
     std::vector<char> result;
     std::vector<char> current;
     int index = n - 1;  // n-1, потому что нумерация начинается с 0
-    getPermutation(tree.getRoot(), index, result, current);
+    getPermutation(tree.getRoot(), &index, &result, &current);
     return result;
 }
