4c0ab1c50dcb667e3ab7466b943f2ebc0201319a
IanaVatagina <150849744+IanaVatagina@users.noreply.github.com>
2024-05-31 23:18:12 +0300

    Update tree.h

--- include/tree.h
+++ include/tree.h
@@ -1,5 +1,28 @@
 // Copyright 2022 NNTU-CS
 #ifndef INCLUDE_TREE_H_
 #define INCLUDE_TREE_H_
+#include <vector>
 
+class Tree {
+ private:
+  struct Node {
+    char value;
+    std::vector<Node*> potomki;
+  };
+  Node *root;
+  std::vector<std::vector<char>> vecPermutations;
+
+  void treeBuild(Node *root, std::vector<char> initValues) {
+    int vecSize = initValues.size();
+    for (int i = 0; i < vecSize; i++) {
+      Node *potomok = new Node; 
+      std::vector<char> leftValues = initValues;
+      int lastElement = leftValues.begin() + i;
+      potomok->value = initValues[i];
+      root->potomki.emplace_back(potomok);
+      leftValues.erase(lastElement);
+      treeBuild(potomok, leftValues);
+    }
+  }
+};
 #endif  // INCLUDE_TREE_H_
