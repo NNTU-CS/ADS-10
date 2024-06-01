ac237713a7cab8314621c1515945cbdf97e401e6
Данила <danila.las.vegas@gmail.com>
2024-06-01 13:00:46 +0300

    .

--- include/tree.h
+++ include/tree.h
@@ -1,5 +1,25 @@
 // Copyright 2022 NNTU-CS
 #ifndef INCLUDE_TREE_H_
 #define INCLUDE_TREE_H_
+#include <vector>
+#include <memory>
 
+class TreeNode {
+public:
+    char value;
+    std::vector<std::shared_ptr<TreeNode>> children;
+
+    TreeNode(char val) : value(val) {}
+};
+
+class Tree {
+public:
+    explicit Tree(const std::vector<char>& elements);
+    std::shared_ptr<TreeNode> getRoot() const { return root; }
+    void buildTree(const std::vector<char>& elements);
+
+private:
+    std::shared_ptr<TreeNode> root;
+    void buildTreeRecursive(std::shared_ptr<TreeNode> node, std::vector<char> elements);
+};
 #endif  // INCLUDE_TREE_H_
