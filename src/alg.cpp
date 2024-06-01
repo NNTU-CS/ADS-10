a3d27af074a1b78aaae10cefe7aff24ff2e0de5c
Данила <danila.las.vegas@gmail.com>
2024-06-01 13:08:35 +0300

    .

--- src/alg.cpp
+++ src/alg.cpp
@@ -5,8 +5,6 @@
 #include  <cstdlib>
 #include  "tree.h"
 
-
-
 void generatePermutations(std::shared_ptr<TreeNode> node, std::vector<char>& current_permutation, std::vector<std::vector<char>>& permutations);
 
 std::vector<char> getPerm(const Tree& tree, int n) {
@@ -44,7 +42,7 @@ void generatePermutations(std::shared_ptr<TreeNode> node, std::vector<char>& cur
 
 Tree::Tree(const std::vector<char>& elements) {
     if (!elements.empty()) {
-        root = std::make_shared<TreeNode>('\0');
+        root = std::make_shared<TreeNode>('\0'); 
         buildTree(elements);
     }
 }
