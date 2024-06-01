851d609840c1bcdceea0d6fd08cbc9c4eff5bfb4
deniskr12 <115426844+deniskr12@users.noreply.github.com>
2024-06-01 16:20:02 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -8,10 +8,8 @@
 std::vector<char> getPerm(const Tree& tree, int n) {
     std::vector<std::vector<char>> permutations = tree.getAllPermutations();
     size_t size = permutations.size();
-    if (n > 0 && n <= size) {
+    if (n > 0 && n <= size)
         return permutations[n - 1];
-    }
-    else {
+    else
         return {};
-    }
 }
