6bbf1215404fd6c3af078300eff194664951055b
IanaVatagina <150849744+IanaVatagina@users.noreply.github.com>
2024-06-01 15:53:50 +0300

    Update tree.h8

--- include/tree.h
+++ include/tree.h
@@ -4,51 +4,46 @@
 #include <vector>
 
 class Tree {
-private:
+ private:
     struct Node {
-        char value;
-        std::vector<Node*> potomki;
-
-        Node(char val) : value(val) {}
+     char value;
+     std::vector<Node *> potomki;
     };
-
-    Node* root;
+    Node *root;
     std::vector<std::vector<char>> vecPermutations;
 
-    void treeBuild(Node* currentNode, const std::vector<char>& initValues) {
-        for (size_t i = 0; i < initValues.size(); i++) {
-            Node* child = new Node(initValues[i]);
-            std::vector<char> leftValues = initValues;
-            leftValues.erase(leftValues.begin() + i);
-            currentNode->potomki.push_back(child);
-            treeBuild(currentNode->potomki.back(), leftValues);
-        }
+    void treeBuild(Node* &root, std::vector<char> initValues) {
+     int vecSize = initValues.size();
+     for (int i = 0; i < vecSize; i++) {
+         Node* potomok = new Node;
+         std::vector<char> leftValues = initValues;
+         potomok->value = initValues[i];
+         root->potomki.push_back(potomok);
+         leftValues.erase(leftValues.begin() + i);
+         treeBuild(root->potomki.back(), leftValues);
+     }
     }
-
     void createPerms(Node* currentNode, const std::vector<char>& initVal) {
         if (currentNode == nullptr) {
             return;
         }
-
         std::vector<char> leftVal = initVal;
         leftVal.push_back(currentNode->value);
-
         if (currentNode->potomki.empty()) {
             vecPermutations.push_back(leftVal);
         }
-
         for (Node* child : currentNode->potomki) {
             createPerms(child, leftVal);
         }
     }
 
-public:
-    explicit Tree(std::vector<char> inputValues) : root(nullptr) {
-        root = new Node(inputValues[0]);
-        treeBuild(root, inputValues);
-        createPerms(root, std::vector<char>());
+ public:
+    explicit Tree(std::vector<char> inputValues): root(nullptr) {
+     std::vector<char> newValVec;
+     root = new Node;
+     treeBuild(root, inputValues);
+     createPerms(root, newValVec);
     }
-
     std::vector<char> getPermByIndex(int i) const {
         if (i < 0 || i >= static_cast<int>(vecPermutations.size())) {
             return std::vector<char>();
@@ -56,5 +51,4 @@ public:
         return vecPermutations[i];
     }
 };
-
 #endif  // INCLUDE_TREE_H_
