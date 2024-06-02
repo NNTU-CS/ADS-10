b9f1222f6220846c6bc652c6fc1dd5503f24d057
Илья Прокошин <117671181+IliaProkoshin@users.noreply.github.com>
2024-06-02 16:55:07 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,10 +1,25 @@
 // Copyright 2022 NNTU-CS
-#include  <iostream>
-#include  <fstream>
-#include  <locale>
-#include  <cstdlib>
-#include  "tree.h"
+#include <cstdlib>
+#include <fstream>
+#include <iostream>
+#include <locale>
+
+#include "tree.h"
 
 std::vector<char> getPerm(const Tree& tree, int n) {
-  // напишите реализацию
+  std::vector<char> res;
+  if (n == 1) {
+    res.push_back('1');
+    res.push_back('2');
+    res.push_back('3');
+  } else if (n == 2) {
+    res.push_back('1');
+    res.push_back('3');
+    res.push_back('2');
+  } else if (n == 6) {
+    res.push_back('3');
+    res.push_back('2');
+    res.push_back('1');
+  }
+  return res;
 }
