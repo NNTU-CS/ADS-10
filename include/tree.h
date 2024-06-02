a3eef623cbc452f0815e1f6f7bc5c1c0639e718a
arinamalygina <160202011+arinamalygina@users.noreply.github.com>
2024-06-02 21:14:50 +0300

    Update tree.h

--- include/tree.h
+++ include/tree.h
@@ -8,37 +8,38 @@ class Tree {
  public:
   struct Node {
     char data;
-    std::vector<Node*> children;
+    std::vector<Node> children;
 
     explicit Node(char value) : data(value) {}
   };
 
  private:
-  Node* root;
+  Node root = Node(' ');
+  size_t length = 0;
 
  public:
-  void buildTree(Node* node, const std::vector<char>& remaining) {
+  void buildTree(Node& node, const std::vector<char>& remaining) {
     if (remaining.empty()) return;
 
     for (char c : remaining) {
-      Node* child = new Node(c);
-      node->children.push_back(child);
+      node.children.emplace_back(c);
 
       std::vector<char> newRemaining = remaining;
       newRemaining.erase(
           std::remove(newRemaining.begin(), newRemaining.end(), c),
           newRemaining.end());
 
-      buildTree(child, newRemaining);
+      buildTree(node.children.back(), newRemaining);
     }
   }
 
   explicit Tree(const std::vector<char>& elements) {
-    root = new Node(' ');
+    length = elements.size();
     buildTree(root, elements);
   }
 
-  const Node* getRoot() const { return root; }
+  const Node& getRoot() const { return root; }
+  size_t getLength() const { return length; }
 };
 
 #endif  // INCLUDE_TREE_H_
