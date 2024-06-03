50f7a709a21506cd1da573f4b78355ebc3a460f8
Arina-Arahis <159996104+Arina-Arahis@users.noreply.github.com>
2024-06-03 14:14:18 +0300

    Update tree.h

--- include/tree.h
+++ include/tree.h
@@ -1,69 +1,72 @@
 // Copyright 2022 NNTU-CS
 #ifndef INCLUDE_TREE_H_
 #define INCLUDE_TREE_H_
-
+#include <iostream>
 #include <vector>
-#include <string>
-#include <algorithm>
-
-class Node {
- public:
+std::vector<char> temp_delete(std::vector<char> num, int ind_to_delete);
+struct Node {
+    char date;
     std::vector<Node*> children;
-    char value;
-
-    explicit Node(char val) : value(val) {}
-
-    ~Node() {
-        for (auto child : children) {
-            delete child;
-        }
-    }
 };
-
 class Tree {
- public:
-    Node* Root1;
-
-    explicit Tree(const std::vector<char>& elements) {
-        Root1 = new Node('\0');
-        buildTree(Root1, elements);
-    }
-
-    ~Tree() {
-        delete Root1;
-    }
-
-    std::vector<std::string> getAllPermutations() const {
-        std::vector<std::string> permutations;
-        genPerm(Root1, "", &permutations);
-        return permutations;
-    }
-
  private:
-    void buildTree(Node* node, const std::vector<char>& elements) {
-        for (char element : elements) {
-            Node* child = new Node(element);
-            node->children.push_back(child);
-            std::vector<char> el = elements;
-            el.erase(std::remove(el.begin(), el.end(), element), el.end());
-            buildTree(child, el);
+    int size;
+
+ public:
+    Node* roter;
+    explicit Tree(std::vector<char> in): size(0) {
+        roter = new Node;
+        roter->date = '0';
+        for (int i = 0; i < in.size(); i++) {
+            Node* newNode = new Node;
+            newNode->date = in[i];
+            roter->children.push_back(newNode);
+        }
+        for (int i = 0; i < in.size(); i++) {
+            std::vector<char> tmp = in;
+            tmp = temp_delete(tmp, i);
+            add_childrens_for_child(tmp, roter->children[i]);
         }
     }
-
-    void genPerm(Node* n, std::string cur, std::vector<std::string>* p) const {
-        if (n->value != '\0') {
-            cur.push_back(n->value);
-        }
-        if (n->children.empty()) {
-            p->push_back(cur);
+    void add_child(Node* parent, char valaid) {
+        Node* q = new Node;
+        q->date = valaid;
+        q->children.push_back(q);
+    }
+    void add_childrens_for_child(std::vector<char> iner, Node* parenti) {
+        if (iner.empty()) {
             return;
         }
-        for (auto child : n->children) {
-            genPerm(child, cur, p);
+        for (int i = 0; i < iner.size(); i++) {
+            Node* newNode = new Node;
+            newNode->date = iner[i];
+            parenti->children.push_back(newNode);
+            std::vector<char> tmp = iner;
+            tmp = temp_delete(tmp, i);
+            add_childrens_for_child(tmp, newNode);
         }
     }
+    std::vector<std::vector<char>> getPerms(int k) const {
+        std::vector<std::vector<char>> perms;
+        std::vector<char> curperm;
+        getPermsRecursive(roter, k, curperm, perms);
+        return perms;
+    }
+    void getPermsRecursive(Node* node, int remaining,
+std::vector<char> curperm, const std::vector<std::vector<char>>& perms) const {
+        if (remaining == 0) {
+            std::vector<std::vector<char>>& nonConstPerms =
+            const_cast<std::vector<std::vector<char>>&>(perms);
+            nonConstPerms.push_back(curperm);
+            return;
+        }
+        for (Node* child : node->children) {
+            std::vector<char> newPerm = curperm;
+            newPerm.push_back(child->date);
+            getPermsRecursive(child, remaining - 1, newPerm, perms);
+        }
+}
 };
-
-std::vector<char> getPerm(const Tree& tree, int index);
+std::vector<char> getPerm(const Tree& tree, int d);
 
 #endif  // INCLUDE_TREE_H_
