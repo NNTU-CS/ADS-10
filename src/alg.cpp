6bd35303c0474b6a4db812889eb6256a900825de
kghstgrl <160274071+kghstgrl@users.noreply.github.com>
2024-06-03 15:53:15 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -7,4 +7,17 @@
 
 std::vector<char> getPerm(const Tree& tree, int n) {
   // напишите реализацию
+  std::string result = tree.getPermutation(tree.getRoot(), n);
+    if (result != "0") {
+        std::vector<char> characters;
+        size_t i = 0;
+        while (i < result.length() && result[i] != '\0') {
+            characters.push_back(result[i]);
+            i++;
+        }
+        return characters;
+    } else {
+        std::vector<char> characters;
+        return characters;
+    }
 }
