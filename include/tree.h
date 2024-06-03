1db8305b340f68c4b11c692557009d6cb710789d
kghstgrl <160274071+kghstgrl@users.noreply.github.com>
2024-06-03 15:48:05 +0300

    Update tree.h

--- include/tree.h
+++ include/tree.h
@@ -2,4 +2,71 @@
 #ifndef INCLUDE_TREE_H_
 #define INCLUDE_TREE_H_
 
+#include <string>
+#include <algorithm>
+#include <vector>
+
+class NodeTree {
+ public:
+    std::vector<NodeTree*> kids;
+    char val;
+    explicit NodeTree(char val) : val(val) {}
+};
+
+class Tree {
+ private:
+    NodeTree* root;
+    void buildTree(NodeTree* nod, std::vector<char> characters) {
+        if (characters.empty()) {
+            return;
+        }
+        for (char character : characters) {
+            NodeTree* child = new NodeTree(character);
+            nod->kids.push_back(child);
+            std::vector<char> remainingCharacters = characters;
+            remainingCharacters.erase(std::remove(remainingCharacters.begin(),
+                remainingCharacters.end(), character), remainingCharacters.end());
+            buildTree(child, remainingCharacters);
+        }
+    }
+public:
+    void freeTree(NodeTree* nod) {
+        int i = 0;
+        while (i < nod->kids.size()) {
+            freeTree(nod->kids[i]);
+            i++;
+        }
+        delete nod;
+    }
+    std::vector<std::string> getPermutations(NodeTree* nod, std::string currentString) const {
+        std::vector<std::string> result;
+        if (nod->kids.empty()) {
+            result.push_back(currentString + nod->val);
+            return result;
+        }
+        int i = 0;
+        while (i < nod->kids.size()) {
+            std::vector<std::string> childPermutations = getPermutations(nod->kids[i], currentString + nod->val);
+            result.insert(result.end(), childPermutations.begin(), childPermutations.end());
+            i++;
+        }
+        return result;
+    }
+    std::string getPermutation(NodeTree* node, int index) const {
+        std::vector<std::string> allPermutations = getPermutations(node, "");
+        if (index < 1 || index > allPermutations.size()) {
+            return "0";
+        }
+        return allPermutations[index - 1];
+    }
+    NodeTree* getRoot() const {
+        return root;
+    }
+    explicit Tree(std::vector<char> characters) {
+        root = new NodeTree('\0');
+        buildTree(root, characters);
+    }
+    ~Tree() = default;
+};
+
 #endif  // INCLUDE_TREE_H_
