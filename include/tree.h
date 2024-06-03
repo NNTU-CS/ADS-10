f18f1f81ed2558b57aa1e4548032dca8d288ec38
kghstgrl <160274071+kghstgrl@users.noreply.github.com>
2024-06-03 17:15:20 +0300

    Update tree.h

--- include/tree.h
+++ include/tree.h
@@ -1,11 +1,9 @@
 // Copyright 2022 NNTU-CS
 #ifndef INCLUDE_TREE_H_
 #define INCLUDE_TREE_H_
-
 #include <string>
 #include <algorithm>
 #include <vector>
-#include <stack>
 
 class NodeTree {
  public:
@@ -15,22 +13,33 @@ class NodeTree {
 };
 
 class Tree {
+ private:
+    NodeTree* root;
+    void buildTree(NodeTree* nod, std::vector<char> characters) {
+        if (characters.empty()) {
+            return;
+        }
+        for (char character : characters) {
+            NodeTree* child = new NodeTree(character);
+            nod->kids.push_back(child);
+            std::vector<char> remainingCharacters = characters;
+            remainingCharacters.erase(std::remove(remainingCharacters.begin(),
+                remainingCharacters.end(), character),
+             remainingCharacters.end());
+            buildTree(child, remainingCharacters);
+        }
+    }
+
  public:
 explicit Tree(std::vector<char> characters) {
         root = new NodeTree('\0');
         buildTree(root, characters);
     }
     void freeTree(NodeTree* nod) {
-        std::stack<NodeTree*> st;
-        st.push(nod);
-        while (!st.empty()) {
-            nod = st.top();
-            st.pop();
-            for (NodeTree* child : nod->kids) {
-                st.push(child);
-            }
-            delete nod;
-        }
+    for (NodeTree* child : nod->kids) {
+        freeTree(child);
+    }
+    delete nod;
     }
     std::vector<std::string> getPermutations(NodeTree* nod,
 const std::string& currentString) const {
@@ -56,24 +65,6 @@ const std::string& currentString) const {
     }
     NodeTree* getRoot() const {
         return root;
-    }
-    ~Tree() = default;
-
- private:
-    NodeTree* root;
-    void buildTree(NodeTree* nod, std::vector<char> characters) {
-        if (nod == nullptr || characters.empty()) {
-            return;
-        }
-        for (char character : characters) {
-            NodeTree* child = new NodeTree(character);
-            nod->kids.push_back(child);
-            std::vector<char> remainingCharacters = characters;
-            remainingCharacters.erase(std::remove(remainingCharacters.begin(),
-                remainingCharacters.end(), character),
-             remainingCharacters.end());
-            buildTree(child, remainingCharacters);
-        }
     }
 };
 
