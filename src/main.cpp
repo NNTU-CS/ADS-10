add90826a6fd4194794e2edc2f00a9fc182e76b6
UlianaKustova <91365332+UlianaKustova@users.noreply.github.com>
2024-06-04 10:52:07 +0300

    Uk

--- src/main.cpp
+++ src/main.cpp
@@ -8,8 +8,8 @@ std::vector<char> getPerm(const Tree& tree, int n);
 int main() {
     vector<char> symbols = { 'A', 'B', 'C' };
     Tree tree(symbols);
-    vector<char> perm1 = getPerm(tr, 1); //первая
-    vector<char> perm2 = getPerm(tr, 3); //третья
+    vector<char> perm1 = getPerm(tree, 1); //первая
+    vector<char> perm2 = getPerm(tree, 3); //третья
     for (char c : perm1) {
         cout << c << " ";
     }
