67bfc3f41e2f40aa196bef0731ebc3185a418a32
LizaZapevalova <160137500+LizaZapevalova@users.noreply.github.com>
2024-06-04 09:33:33 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,6 +1,10 @@
+// Copyright 2022 NNTU-CS
+#include  <iostream>
+#include  <fstream>
+#include  <locale>
+#include  <cstdlib>
 #include  "tree.h"
 std::vector<char> getPerm(const Tree& tree, int n) {
-  std::vector<std::vector<char> > perms = tree.getPermutations();
-  if (perms.size() >= n) return perms[n - 1];
-  return {};
+  // напишите реализацию
+  return tree.getConst(n - 1);
 }
