29cd0975145880f3a87fa078f1e346d61d1df729
UlianaKustova <91365332+UlianaKustova@users.noreply.github.com>
2024-06-04 10:38:59 +0300

    Uk

--- include/tree.h
+++ include/tree.h
@@ -5,6 +5,61 @@
 using namespace std;
 class tree {
 private:
+    struct Node {
+        vector<Node*> ch;
+        char value;
+    };
+    Node* root;
+    vector<vector<char>> constant;
+
+public:
+    tree(vector<char> entr);
+    void buildTree(Node* root, vector<char> entr);
+    void constan(Node* root, const vector<char> vect);
+    vector<char> getConst(int i)const;
 };
+
+tree::tree(vector<char> entr) {
+    vector<char> vec;
+    root = new Node;
+    buildTree(root, entr);
+    constan(root, vec);
+}
+
+void tree::buildTree(Node* root, vector<char> entr) {
+    if (entr.size() == 0) {//если вектор пустой?
+        return;
+    }
+    else {
+        for (int i = 0; i < entr.size(); i++) {
+            vector<char> temp = entr;
+            Node* nextU = new Node;
+            nextU->value = entr[i];
+            root->ch.push_back(nextU);
+            temp.erase(temp.begin() + i);
+            buildTree(root->ch.back(), temp);
+        }
+    }
+}
+
+void tree::constan(Node* root, const vector<char> vect) {
+    vector<char> temp1 = vect;
+    for (int i = 0; i < root->ch.size(); i++) {
+        temp1.push_back(root->ch[i]->value);
+        if (root->ch[i]->ch.size() == 0) {
+            constant.push_back(temp1);
+        }
+        constan(root->ch[i], temp1);
+        temp1.pop_back();
+    }
+}
+
+vector<char> tree::getConst(int i)const {
+    if (i > constant.size() - 1) {
+        vector<char> null;
+        return null;
+    }
+    return constant[i];
+}
 
 #endif  // INCLUDE_TREE_H_
