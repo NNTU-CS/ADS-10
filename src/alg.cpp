f1630b0ecc49c4803e37a4f6417ee31c884fc2dd
Почемучка <159887975+Maria-Kipurova@users.noreply.github.com>
2024-06-06 10:46:45 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -5,6 +5,17 @@
 #include  <cstdlib>
 #include  "tree.h"
 
+int factorial(int n) {
+    if (n == 0)
+        return 1;
+    return n * factorial(n - 1);
+}
+
 std::vector<char> getPerm(const Tree& tree, int n) {
-  // напишите реализацию
+    std::vector<char> result = {};
+    if (n < 0 || factorial(tree.GetSize()) < n)
+      return {};
+    result = tree.GetPerm(n-1);
+    result.erase(result.begin());
+    return result;
 }
