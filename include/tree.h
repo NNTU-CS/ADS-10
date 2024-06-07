cb8e983756991592d926d0ab813321cb34ffb8dd
flapik19 <selivanovvlad3211@gmail.com>
2024-06-08 02:25:48 +0300

    .

--- include/tree.h
+++ include/tree.h
@@ -1,4 +1,3 @@
-// Copyright 2022 NNTU-CS
 #ifndef INCLUDE_TREE_H_
 #define INCLUDE_TREE_H_
 #include <vector>
@@ -8,50 +7,59 @@ class Tree {
     struct Node {
         std::vector<Node*> child;
         char value;
+        Node(char val) : value(val) {}
     };
     Node* root;
     std::vector<std::vector<char>> consta;
+
  public:
-    explicit Tree(std::vector<char> in) {
-        root = new Node;
+    explicit Tree(const std::vector<char>& in) {
+        root = new Node('\0');
         std::vector<char> used;
-        buildTree(root,in);
-        Perm(root,used);
+        buildTree(root, in);
+        Perm(root, used);
     }
 
-    void buildTree(Node* root, std::vector<char> in) {
-        if (!in.empty()) {
-            for (int i = 0; i < in.size(); i++) {
-                std::vector<char> temp;
-                Node* next = new Node;
-                next -> value;
-                root -> child.push_back(next);
-                temp.erase(temp.begin() + 1);
-                buildTree(root -> child.back(), temp);
-                }
-            } else {
-                return;
-            } 
+    ~Tree() {
+        clearTree(root);
     }
-    
-    void Perm(Node* root, const std::vector<char> used) {
-    std::vector<char> copy = used;
-    for (int i = 0; i < root->child.size(); i++) {
-      copy.push_back(root->child[i]->value);
-        if (root->child[i]->child.empty()) {
-          consta.push_back(copy);
+
+    void clearTree(Node* node) {
+        for (Node* child : node->child) {
+            clearTree(child);
         }
-      Perm(root->child[i], copy);
-      copy.pop_back();
+        delete node;
     }
+
+    void buildTree(Node* node, std::vector<char> in) {
+        if (!in.empty()) {
+            for (size_t i = 0; i < in.size(); ++i) {
+                std::vector<char> temp(in.begin(), in.end());
+                temp.erase(temp.begin() + i);
+                Node* next = new Node(in[i]);
+                node->child.push_back(next);
+                buildTree(next, temp);
+            }
+        }
+    }
+
+    void Perm(Node* node, std::vector<char> used) {
+        for (size_t i = 0; i < node->child.size(); ++i) {
+            used.push_back(node->child[i]->value);
+            if (node->child[i]->child.empty()) {
+                consta.push_back(used);
+            }
+            Perm(node->child[i], used);
+            used.pop_back();
+        }
     }
 
     std::vector<char> GetPerm(int i) const {
-    if (i > consta.size() - 1) {
-      std::vector<char> null;
-      return null;
+        if (i < 0 || static_cast<size_t>(i) >= consta.size()) {
+            return std::vector<char>();
+        }
+        return consta[i];
     }
-    return consta[i];
-  }
 };
+
 #endif  // INCLUDE_TREE_H_
