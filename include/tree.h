861be5cb5ccca147ee1e9fd007b9a2777e1d1341
artemkosterin <160392965+artemkosterin@users.noreply.github.com>
2024-06-08 01:56:17 +0300

    Update tree.h

--- include/tree.h
+++ include/tree.h
@@ -1,5 +1,23 @@
 // Copyright 2022 NNTU-CS
 #ifndef INCLUDE_TREE_H_
 #define INCLUDE_TREE_H_
+#include <cstdint>
+#include <string>
+#include <vector>
 
+class Tree {
+ private:
+    struct Node;
+    std::vector<std::string> permutations;
+    Node* root;
+
+    void buildTree(Node* node, std::vector<char> elements);
+
+    void generatePermutations(Node* node, std::string current);
+
+ public:
+    explicit Tree(const std::vector<char>& elements);
+
+    std::string operator[](unsigned int index) const;
+};
 #endif  // INCLUDE_TREE_H_
