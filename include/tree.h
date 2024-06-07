67424f9438bf6a8d1dced58dd407343eebdb4614
VarvaraBelova23 <160231633+VarvaraBelova23@users.noreply.github.com>
2024-06-07 21:25:18 +0300

    Update tree.h

--- include/tree.h
+++ include/tree.h
@@ -7,12 +7,12 @@
 #include <algorithm>
 
 struct Node {
-    char vpx;
-    bool isp = false;
-    std::vector<Node*> point;
-    explicit Node(char val) : vpx(val) {}
+    char value;
+    std::vector<Node*> children;
+
+    Node(char val) : value(val) {}
     ~Node() {
-        for (auto child : point) {
+        for (auto child : children) {
             delete child;
         }
     }
@@ -20,68 +20,16 @@ struct Node {
 
 class Tree {
  private:
-    Node* cor;
-    typedef std::vector<std::vector<char>> PermsType;
-    PermsType p;
-    void insertRecursive(Node* cor, const std::vector<char>& vec); 
-    void insert(Node* cor, const std::vector<char>& vec); 
-    void generatePerms(Node* cor, std::vector<char> vec);
+    Node* root;
+    std::vector<std::vector<char>> permutations;
+    void buildTree(Node* current, const std::vector<char>& elements);
+    void traverseTree(Node* current, std::vector<char>& permutation);
 
  public:
-    explicit Tree(const std::vector<char>& vec);
+    explicit Tree(const std::vector<char>& elements);
     ~Tree();
-    std::vector<std::vector<char>> getP() const {
-        return p;
-    }
+    std::vector<std::vector<char>> getPermutations() const;
+    std::vector<char> getPermutation(int n) const;
 };
-
-Tree::Tree(const std::vector<char>& vec) {
-    cor = new Node;
-    cor->isp = true;
-    insert(cor, vec);
-    std::vector<char> temp;
-    generatePerms(cor, temp);
-}
-
-Tree::~Tree() {
-    delete cor;
-}
-
-void Tree::insertRecursive(Node* cor, const std::vector<char>& vec) {
-    if (vec.empty()) {
-        return;
-    }
-
-    char c = *std::min_element(vec.begin(), vec.end());
-    Node* s = new Node(c);
-    cor->point.push_back(s);
-    vec.erase(std::find(vec.begin(), vec.end(), c));
-    insertRecursive(s, vec);
-}
-
-void Tree::insert(Node* cor, const std::vector<char>& vec) {
-    for (char c : vec) {
-        Node* s = new Node(c); 
-        cor->point.push_back(s);
-
-        std::vector<char> remainingChars(vec);
-        remainingChars.erase(std::find(remainingChars.begin(),
-            remainingChars.end(), c));
-
-        insertRecursive(s, remainingChars);
-    }
-}
-
-void Tree::generatePerms(Node* cor, std::vector<char> vec) {
-    if (!cor->isp) vec.push_back(cor->vpx);
-    if (cor->point.empty()) { 
-        p.push_back(vec);
-    } else { 
-        for (Node* child : cor->point) { 
-            std::vector<char> newVec = vec;
-            generatePerms(child, newVec); 
-        } 
-    }
-}
 
 #endif  // INCLUDE_TREE_H_
