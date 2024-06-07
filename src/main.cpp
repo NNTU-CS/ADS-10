c89dc70fa2fcac244e99e58b673e6e2a8d9df316
VarvaraBelova23 <160231633+VarvaraBelova23@users.noreply.github.com>
2024-06-07 17:25:53 +0300

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,16 @@
 // Copyright 2022 NNTU-CS
 #include "tree.h"
+#include <vector>
 #include <iostream>
+
+std::vector<char> getPerm(const Tree& tree, int n);
+
+void printVector(const std::vector<char>& vec) { 
+    for (char ch : vec) { 
+        std::cout << ch; 
+    } 
+    std::cout << std::endl; 
+}
 
 int main() {
     // определяем входной вектор для дерева
@@ -8,12 +18,13 @@ int main() {
     // создаем дерево
     Tree tree(in);
     // запрашиваем первую перестановку
-    std::vector<char> result1 = getPerm(tree, 1);  // 123
-    for (char c : result1) std::cout << c;
-    std::cout << std::endl;
+    std::vector<char> result1 = getPerm(tree, 1);  //  123 
+    std::cout << "Permutation #" << 1 << ": "; 
+    printVector(result1);
     // запрашиваем вторую перестановку
-    std::vector<char> result2 = getPerm(tree, 2);  // 132
-    for (char c : result2) std::cout << c;
-    std::cout << std::endl;
+    std::vector<char> result2 = getPerm(tree, 2);  //  132 
+    std::cout << "Permutation #" << 2 << ": "; 
+    printVector(result2);
+    
     return 0;
 }
