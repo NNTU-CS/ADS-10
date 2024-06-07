d43febd54390d138a18a181d0b060366afabefb7
VarvaraBelova23 <160231633+VarvaraBelova23@users.noreply.github.com>
2024-06-07 21:09:27 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,13 +1,13 @@
-#include <iostream>
-#include <fstream>
-#include <locale>
-#include <cstdlib>
-#include "tree.h"
+#include  <iostream>
+#include  <fstream>
+#include  <locale>
+#include  <cstdlib>
+#include  "tree.h"
 
-std::vector<char> getPerm(const Tree& tree, int n) {
+std::vector<char> getPerm(const Tree& tree, int n) { 
     std::vector<std::vector<char>> p = tree.getP();
     if (n <= p.size()) {
         return p[n - 1];
-    }
+    } 
     return {};
 }
