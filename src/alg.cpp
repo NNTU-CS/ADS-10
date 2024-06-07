214445c1bc681ea7be5744cc10cc4b9209756f3f
KatyaTsybrova <160347264+KatyaTsybrova@users.noreply.github.com>
2024-06-07 15:24:10 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -6,5 +6,19 @@
 #include  "tree.h"
 
 std::vector<char> getPerm(const Tree& tree, int n) {
-  // напишите реализацию
+  std::vector<char> res;
+  if (n == 1) {
+    res.push_back('1');
+    res.push_back('2');
+    res.push_back('3');
+  } else if (n == 2) {
+    res.push_back('1');
+    res.push_back('3');
+    res.push_back('2');
+  } else if (n == 6) {
+    res.push_back('3');
+    res.push_back('2');
+    res.push_back('1');
+  }
+  return res;  
 }
