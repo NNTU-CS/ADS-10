1c1c4fd2a4cec76883d3d497b0da2ad80a817ddc
KatyaTsybrova <160347264+KatyaTsybrova@users.noreply.github.com>
2024-06-07 16:24:55 +0300

    Update tree.h

--- include/tree.h
+++ include/tree.h
@@ -1,49 +1,30 @@
 // Copyright 2022 NNTU-CS
 #ifndef INCLUDE_TREE_H_
 #define INCLUDE_TREE_H_
-struct TreeNode {
-    vector<TreeNode*> children;
-    int value;
+
+#include <vector>
+
+class Tree {
+ private:
+  std::vector<char> vec;
+  int length = 0, perm = 0;
+
+ public:
+  explicit Tree(const std::vector<char> vector) : vec{vector} {
+    vec = vector;
+    for (int i = 0; i < vec.size(); i++) {
+      length++;
+    }
+    if (length) {
+      perm = 1;
+    }
+    for (int i = 2; i <= length; i++) {
+      perm *= i;
+    }
+  }
+
+  int get_length() const { return length; }
+  int get_perm() const { return perm; }
 };
 
-void buildTree(TreeNode* root, vector<int> elements) {
-    if (elements.empty()) {
-        return;
-    }
-
-    int numChildren = elements.size();
-    root->value = elements[0];
-
-    for (int i = 1; i < numChildren; ++i) {
-        TreeNode* child = new TreeNode();
-        child->value = elements[i];
-        root->children.push_back(child);
-        buildTree(child, {elements[i + 1], elements[i + 2]});
-        ++i;
-    }
-}
-
-void generatePermutations(TreeNode* root, vector<string>& permutations, string current) {
-    if (!root) {
-        permutations.push_back(current);
-        return;
-    }
-
-    current += to_string(root->value);
-
-    for (TreeNode* child : root->children) {
-        generatePermutations(child, permutations, current);
-    }
-}
-
-string getPermutationByNumber(TreeNode* root, int number) {
-    vector<string> permutations;
-    generatePermutations(root, permutations, "");
-
-    if (number <= permutations.size()) {
-        return permutations[number - 1];
-    }
-
-    return "";
-}
 #endif  // INCLUDE_TREE_H_
