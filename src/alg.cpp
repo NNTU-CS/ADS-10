2e170301135ac0254907ae79228fbefd6b04095d
putintsevajulia <160262047+putintsevajulia@users.noreply.github.com>
2024-06-08 21:33:15 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -4,7 +4,21 @@
 #include  <locale>
 #include  <cstdlib>
 #include  "tree.h"
+#include <vector>
 
 std::vector<char> getPerm(const Tree& tree, int n) {
-  // напишите реализацию
+    std::vector<char> permutation;
+    const Node* current = node;
+
+    while (!current->children.empty()) {
+        int total_permutations = current->children[0]->num_permutations;
+
+        int index = n / (total_permutations / current->children.size());
+        n %= (total_permutations / current->children.size());
+
+        permutation.push_back(current->children[index]->data);
+        current = current->children[index];
+    }
+
+    return permutation;
 }
