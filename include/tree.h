65aa91f74025639c2f48d88737ed861223b9f9e3
mortymerrrr <160071899+mortymerrrr@users.noreply.github.com>
2024-06-08 23:46:19 +0300

    b58.h

--- include/tree.h
+++ include/tree.h
@@ -2,4 +2,51 @@
 #ifndef INCLUDE_TREE_H_
 #define INCLUDE_TREE_H_
 
+#include <algorithm>
+#include <iostream>
+#include <vector>
+
+class PermutationTree {
+private:
+    struct Node {
+        char value;
+        std::vector<Node*> children;
+    };
+
+    Node* root = nullptr;
+    std::vector<char> permutation;
+    void buildPermutationTree(std::vector<char> elements, Node* node) {
+        for (auto element : elements) {
+            Node* child = new Node;
+            child->value = element;
+            node->children.push_back(child);
+            std::vector<char> remaining = elements;
+            remaining.erase(std::remove(remaining.begin(),
+                remaining.end(), element), remaining.end());
+            buildPermutationTree(remaining, child);
+        }
+    }
+    void generatePermutations(Node* node) {
+        if (node->children.empty()) {
+            permutation.push_back(node->value);
+            permutations.push_back(permutation);
+            permutation.pop_back();
+        }
+        for (Node* child : node->children) {
+            permutation.push_back(node->value);
+            generatePermutations(child);
+            permutation.pop_back();
+        }
+    }
+
+public:
+    std::vector<std::vector<char>> permutations;
+    explicit PermutationTree(std::vector<char> elements) {
+        root = new Node;
+        root->value = 0;
+        buildPermutationTree(elements, root);
+        generatePermutations(root);
+    }
+};
+
 #endif  // INCLUDE_TREE_H_
