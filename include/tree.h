03e432281e56246644a48782a9ba226b754d5353
steelover <150594943+steelover@users.noreply.github.com>
2024-06-08 16:41:25 +0300

    Update tree.h

--- include/tree.h
+++ include/tree.h
@@ -1,5 +1,27 @@
 // Copyright 2022 NNTU-CS
 #ifndef INCLUDE_TREE_H_
 #define INCLUDE_TREE_H_
+#include <vector>
+class Tree {
+ private:
+    std::vector<char> vec;
+    int length = 0, perm = 0;
 
+ public:
+    explicit Tree(const std::vector<char> vector) : vec{vector} {
+        vec = vector;
+        for (int i = 0; i < vec.size(); i++) {
+            length++;
+        }
+        if (length > 0) {
+            perm = 1;
+        }
+
+        for (int i = 2; i <= length; i++) {
+            perm *= i;
+        }
+    }
+    int get_length() const { return length; }
+    int get_perm() const { return perm; }
+};
 #endif  // INCLUDE_TREE_H_
