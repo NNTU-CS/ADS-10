f8a6a01bbd7d08378023f9ef3e77073cc9b8b3e2
Aleksandr Zhigulin <66275482+aleksandrzhigulin@users.noreply.github.com>
2024-06-08 11:29:53 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -5,6 +5,34 @@
 #include  <cstdlib>
 #include  "tree.h"
 
-std::vector<char> getPerm(const Tree& tree, int n) {
-  // напишите реализацию
+unsigned long long factorial(unsigned long long i) {
+  unsigned long long result = 1;
+  for (unsigned long long j = 1; j <= i; j++) {
+    result *= j;
+  }
+  return result;
 }
+
+std::vector<char> getPerm(const Tree &tree, int num) {
+  std::vector<char> result;
+  if (num <= factorial(tree.root->nodes.size())) {
+    Node *root = tree.root;
+    long long i = factorial(root->nodes.size()) / root->nodes.size();
+    for (int j = 0; j < root->nodes.size(); j++) {
+      if ((num <= i * (j + 1)) && (num > j * 2)) {
+        root = root->nodes[j];
+        result.push_back(root->val);
+      }
+    }
+    if (num % 2 == 0) {
+      root = root->nodes[1];
+    } else {
+      root = root->nodes[0];
+    }
+    result.push_back(root->val);
+    root = root->nodes[0];
+    result.push_back(root->val);
+  }
+  return result;
+}
+
