65d46e46c099793c9f2cac76231b232c38d32ab9
Ilona <150517376+LunyaLp@users.noreply.github.com>
2024-06-09 14:52:11 +0300

    Update tree.h

--- include/tree.h
+++ include/tree.h
@@ -13,34 +13,34 @@ class Tree {
     std::vector<std::vector<char>> permutations;
 
     void buildTree(Node *node, std::vector<char> val) {
-        if (!node) {
-            node = new Node;
-        }
-        if (val.empty()) {
-            return;
-        }
-        for (int i = 0; i < val.size(); i++) {
-            std::vector<char> rValues = val;
-            Node *perem = new Node;
-            perem->val = val[i];
-            node->perems.push_back(perem);
-            rValues.erase(rValues.begin() + i);
-            buildTree(node->perems.back(), rValues);
-        }
+     if (!node) {
+      node = new Node;
+     }
+     if (val.empty()) {
+      return;
+     }
+     for (int i = 0; i < val.size(); i++) {
+      std::vector<char> rValues = val;
+      Node *perem = new Node;
+      perem->value = val[i];  // Заменяем perem->val на perem->value
+      node->perems.push_back(perem);
+      rValues.erase(rValues.begin() + i);
+      buildTree(node->perems.back(), rValues);
+     }
     }
-    void generatePermutations(Node* node, const std::vector<char>& current) {
-        std::vector<char> upCurrent = current;
-        for (int i = 0; i < node->perems.size(); i++) {
-            upCurrent.push_back(node->perems[i]->val);
-            if (node->perems[i]->perems.empty()) {
-                if (current.size() != 1) {
-                    permutations.push_back(upCurrent);
-                }
-            }
-            generatePermutations(node->perems[i], upCurrent);
-            upCurrent.pop_back();
-        }
-    }
+void generatePermutations(Node* node, std::vector<char>& current) {
+ std::vector<char> upCurrent = current;
+ for (int i = 0; i < node->perems.size(); i++) {
+  upCurrent.push_back(node->perems[i]->value);  // Заменяем node->perems[i]->val на node->perems[i]->value
+  if (node->perems[i]->perems.empty()) {
+   if (current.size() != 1) {
+    permutations.push_back(upCurrent);
+   }
+  }
+  generatePermutations(node->perems[i], upCurrent);
+  upCurrent.pop_back();
+ }
+}
 
  public:
     explicit Tree(std::vector<char> val): root(nullptr) {
