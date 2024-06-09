5ba1e17abfe28d9b082bee696fbdb81ecd62072f
Rodyaaaa <160117492+Rodyaaaa@users.noreply.github.com>
2024-06-09 23:46:39 +0300

    Update tree.h

--- include/tree.h
+++ include/tree.h
@@ -2,4 +2,28 @@
 #ifndef INCLUDE_TREE_H_
 #define INCLUDE_TREE_H_
 
+#include <vector>
+#include <memory>
+
+class TreeNode {
+public:
+    char value;
+    std::vector<std::shared_ptr<TreeNode>> children;
+
+    TreeNode(char val) : value(val) {}
+};
+
+class Tree {
+public:
+    Tree(const std::vector<char>& elements);
+    std::vector<std::vector<char>> generateAllPermutations() const;
+
+private:
+    std::shared_ptr<TreeNode> root;
+    void buildTree(std::shared_ptr<TreeNode> node, std::vector<char> elements);
+    void traverseTree(std::shared_ptr<TreeNode> node, std::vector<char>& currentPermutation, std::vector<std::vector<char>>& permutations) const;
+};
+
+std::vector<char> getPerm(const Tree& tree, int n);
+
 #endif  // INCLUDE_TREE_H_
