14b2faa249292feaf3903a69408e6bb8ba02b1f6
Dobro <76173176+OlesuaDobro@users.noreply.github.com>
2024-06-09 16:10:00 +0300

    Update tree.h

--- include/tree.h
+++ include/tree.h
@@ -19,7 +19,7 @@ std::vector<std::vector<char>> constan;
         buildTree(root, entr);
         constan(root, vec);
     }
-    std::vector<char> getPerm(constan Tree& tree, int n) {
+    std::vector<char> getPerm(const Tree& tree, int n) {
         if (n == 0) {
             return permutation;
         }
@@ -33,9 +33,10 @@ std::vector<std::vector<char>> constan;
         }
         return {};
     }
-std::vector<char> getperm(constan Tree& tree, int n) {
+std::vector<char> getperm(const Tree& tree, int n) {
     std::vector<char> permutation;
-    return Tree::getperm(Tree::Node* constan&, int&, std::vector<char>&) constan);
+    return Tree::getperm(Tree::Node* constan&, int&,
+     std::vector<char>&) constan);
 }
 };
 
