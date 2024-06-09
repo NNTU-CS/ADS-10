5178c5931c75144d6f243da34e8d17002804e445
andsvee <160344384+andsvee@users.noreply.github.com>
2024-06-09 15:22:26 +0300

    Update tree.h

--- include/tree.h
+++ include/tree.h
@@ -1,5 +1,19 @@
 // Copyright 2022 NNTU-CS
 #ifndef INCLUDE_TREE_H_
 #define INCLUDE_TREE_H_
+#include <cstdint>
+#include <string>
+#include <vector>
 
+class Tree {
+ private:
+    struct Node;
+    std::vector<std::string> permutations;
+    Node* root;
+    void buildTree(Node* senior, std::vector<char> trail);
+    void generatePermutations(Node* senior, std::string symb);
+ public:
+    explicit Tree(const std::vector<char>);
+    std::string operator[] (unsigned int i) const;
+};
 #endif  // INCLUDE_TREE_H_
