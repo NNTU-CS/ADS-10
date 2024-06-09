3dad911a84b758d19c1665166f79f22afc6994b6
TailorOfEnbizaka <160338576+TailorOfEnbizaka@users.noreply.github.com>
2024-06-09 16:21:41 +0300

    Update tree.h

--- include/tree.h
+++ include/tree.h
@@ -5,43 +5,44 @@
 #include <algorithm>
 class Tree {
 private:
-	struct Node {
-		std::vector<Node*> nodes;
-		char v;
-		explicit Node(char value) : v(value) {}
-	};
-	Node* root;
-	std::vector<std::vector<char>>perestanovki;
-	void newTree(const std::vector<char>& counter, Node* ancestors) {
-		char c;
-		for (char c : counter) {
-			Node* descendants = new Node(c);
-			ancestors->nodes.push_back(descendants);
-			std::vector<char>c_counter(counter);
-			c_counter.erase(std::find(c_counter.begin(), c_counter.end(), c));
-		}
-	}
-	void Perestanovki(std::vector<char>p, Node* roots) {
-		p.push_back(roots->v);
-		if (root->nodes.empty()) {
-			perestanovki.push_back(p);
-		}
-		for (Node* descendants : root->nodes) {
-			Perestanovki(p, descendants);
-		}
-		p.pop_back();
-	}
+  struct Node {
+    char v;
+    std::vector<Node*> nodes;
+    explicit Node(char val) : v(val) {}
+  };
+  Node* root;
+  std::vector<std::vector<char>> perestanovki;
+  void newTreeee(const std::vector<char>& counter, Node* ancestors) {
+    for (char c : counter) {
+      Node* descendants = new Node(c);
+      ancestors->nodes.push_back(descendants);
+      std::vector<char> c_counter(counter);
+      c_counter.erase(std::find(c_counter.begin(), c_counter.end(), c));
+      newTreeee(c_counter, descendants);
+    }
+  }
+  void GenaBukin(Node* root, std::vector<char> p) {
+    p.push_back(root->v);
+    if (root->nodes.empty()) {
+      perestanovki.push_back(p);
+    }
+    for (Node* descendants : root->nodes) {
+      GenaBukin(descendants, p);
+    }
+    p.pop_back();
+  }
+
 public:
-	explicit Tree(const std::vector<char>& counter) {
-		root = new Node('.');
-		newTree(counter, root);
-		Perestanovki(root, {});
-	}
-	const int Size() const {
-		return root->nodes.size();
-	}
-	std::vector<char>Got(int i) const {
-		return perestanovki[i];
-	}
+  explicit Tree(const std::vector<char>& counter) {
+    root = new Node('^');
+    newTreeee(counter, root);
+    GenaBukin(root, {});
+  }
+  const int Razmer() const {
+    return root->nodes.size();
+  }
+  std::vector<char> GetPerm(int n) const {
+    return perestanovki[n];
+  }
 };
 #endif  // INCLUDE_TREE_H_
