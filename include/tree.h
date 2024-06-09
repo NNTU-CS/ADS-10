34a33b27f8423ab8e0ca7081a1586b010e19c59c
BaevGeorgy55 <160393440+BaevGeorgy55@users.noreply.github.com>
2024-06-09 19:26:30 +0300

    Update tree.h

--- include/tree.h
+++ include/tree.h
@@ -1,5 +1,19 @@
 // Copyright 2022 NNTU-CS
 #ifndef INCLUDE_TREE_H_
 #define INCLUDE_TREE_H_
+#include <cstdint>
+#include <string>
+#include <vector>
 
+class Drevo {
+ private:
+    struct Nd;
+    std::vector<std::string> Premut;
+    Nd* prava;
+    void buildDrevo(Node* sir, std::vector<char> sled);
+    void generatePermut(Node* sir, std::string Sim);
+ public:
+    explicit Drevo(const std::vector<char>);
+    std::string operator[] (unsigned int y) const;
+};
 #endif  // INCLUDE_TREE_H_
