f3d1164d866b81b2086f64a4db9eafa4712a944b
Kotyara21 <73665937+Kotyara21@users.noreply.github.com>
2024-06-09 21:18:52 +0300

    Update tree.h

--- include/tree.h
+++ include/tree.h
@@ -2,4 +2,23 @@
 #ifndef INCLUDE_TREE_H_
 #define INCLUDE_TREE_H_
 
+#include <cstdint>
+#include <string>
+#include <vector>
+
+class Tree {
+ private:
+    struct Node;
+    std::vector<std::string> repl;
+    Node* fort;
+
+    void createTree(Node* fort, std::vector<char> trail);
+
+    void generatePermutations(Node* fort, std::string symb);
+
+ public:
+    explicit Tree(const std::vector<char>);
+
+    std::string operator[] (unsigned int i) const;
+};
 #endif  // INCLUDE_TREE_H_
