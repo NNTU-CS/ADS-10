351293eb855d584f342ec0e0611dbb76d3c152b7
laal28 <129623228+laal28@users.noreply.github.com>
2024-06-09 11:15:39 +0300

    Implemented

--- src/alg.cpp
+++ src/alg.cpp
@@ -5,6 +5,13 @@
 #include  <cstdlib>
 #include  "tree.h"
 
-std::vector<char> getPerm(const Tree& tree, int n) {
-  // напишите реализацию
+std::vector<char> getPerm(const Tree& tree, int index) {
+    std::vector<std::string> allPermutations = tree.getAllPermutations();
+
+    if (index > 0 && index <= allPermutations.size()) {
+        std::string perm = allPermutations[index - 1];
+        return std::vector<char>(perm.begin(), perm.end());
+    } else {
+        return std::vector<char>();
+    }
 }
