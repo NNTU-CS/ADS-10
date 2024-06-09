4457df2dce8405925376703a5e03f61713d31504
VarvaraBelova23 <160231633+VarvaraBelova23@users.noreply.github.com>
2024-06-09 19:30:59 +0300

    Update tree.h

--- include/tree.h
+++ include/tree.h
@@ -2,34 +2,79 @@
 #ifndef INCLUDE_TREE_H_
 #define INCLUDE_TREE_H_
 
-#include <iostream>
 #include <vector>
-#include <algorithm>
-
-struct Node {
-    char value;
-    std::vector<Node*> children;
-
-    Node(char val) : value(val) {}
-    ~Node() {
-        for (auto child : children) {
-            delete child;
-        }
-    }
-};
 
 class Tree {
  private:
+    struct Node {
+        std::vector<Node*> childrenperm;
+        char x;
+
+        Node(char val) : x(val) {}
+
+        ~Node() {
+            for (auto deti : childrenperm) {
+                delete deti;
+            }
+        }
+    };
+
     Node* root;
     std::vector<std::vector<char>> permutations;
-    void buildTree(Node* current, const std::vector<char>& elements);
-    void traverseTree(Node* current, std::vector<char>& permutation);
+
+    void Recursive(Node* node, const std::vector<char>& values);
+    void generatePermutationsRecursive(Node* node, std::vector<char> current);
 
  public:
-    explicit Tree(const std::vector<char>& elements);
+    explicit Tree(const std::vector<char>& values);
     ~Tree();
-    std::vector<std::vector<char>> getPermutations() const;
-    std::vector<char> getPermutation(int n) const;
+
+    std::vector<char> getPermutation(int index) const;
 };
+
+Tree::Tree(const std::vector<char>& values) {
+    root = new Node(0);
+    Recursive(root, values);
+    std::vector<char> current;
+    generatePermutationsRecursive(root, current);
+}
+
+Tree::~Tree() {
+    delete root;
+}
+
+void Tree::Recursive(Node* node, const std::vector<char>& values) {
+    if (values.empty()) {
+        return;
+    }
+
+    for (size_t i = 0; i < values.size(); ++i) {
+        std::vector<char> nextElems(values.begin(), values.end());
+        nextElems.erase(nextElems.begin() + i);
+
+        Node* deti = new Node(values[i]);
+        node->childrenperm.push_back(deti);
+
+        Recursive(deti, nextElems);
+    }
+}
+
+void Tree::generatePermutationsRecursive(Node* node, std::vector<char> current) {
+    for (int i = 0; i < node->childrenperm.size(); i++) {
+        current.push_back(node->childrenperm[i]->x);
+        if (node->childrenperm[i]->childrenperm.empty()) {
+            permutations.push_back(current);
+        }
+        generatePermutationsRecursive(node->childrenperm[i], current);
+        current.pop_back();
+    }
+}
+
+std::vector<char> Tree::getPermutation(int index) const {
+    if (index < 0 || index >= permutations.size()) {
+        return std::vector<char>();
+    }
+    return permutations[index];
+}
 
 #endif  // INCLUDE_TREE_H_
