f9cca89ffb55ef37734123494b89f21e165cccf9
Dobro <76173176+OlesuaDobro@users.noreply.github.com>
2024-06-09 14:18:38 +0300

    Update tree.h

--- include/tree.h
+++ include/tree.h
@@ -2,4 +2,42 @@
 #ifndef INCLUDE_TREE_H_
 #define INCLUDE_TREE_H_
 
+class Node {
+public:
+    char data;
+    std::vector<Node*> children;
+    Node(char value) {
+        data = value;
+    }
+};
+class Tree {
+public:
+    Node* root;
+    Tree(std::vector<char>& input) {
+        root = new Node('\0');
+        for (char value : input) {
+            Node* child = new Node(value);
+            root->children.push_back(child);
+        }
+    }
+    std::vector<char> getperm(Node* node, int& n, std::vector<char>& permutation) {
+        if (n == 0) {
+            return permutation;
+        }
+        for (Node* child : node->children) {
+            permutation.push_back(child->data);
+            std::vector<char> result = getperm(child, --n, permutation);
+            if (!result.empty()) {
+                return result;
+            }
+            permutation.pop_back();
+        }
+        return {};
+    }
+};
+std::vector<char> getperm(const Tree& tree, int n) {
+    std::vector<char> permutation;
+    return tree.getperm(tree.root, n, permutation);
+}
+
 #endif  // INCLUDE_TREE_H_
