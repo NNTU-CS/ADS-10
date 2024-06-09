39c27478093e973a4d97ce2af6c392c1236e4b98
hellboy2364 <160237839+hellboy2364@users.noreply.github.com>
2024-06-09 20:57:19 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -5,6 +5,16 @@
 #include  <cstdlib>
 #include  "tree.h"
 
+std::vector<char> udal(std::vector<char> chis, int por_nom) {
+    if (por_nom >= 0 && por_nom < chis.size()) {
+        chis.erase(chis.begin() + por_nom);
+    }
+    return chis;
+}
 std::vector<char> getPerm(const Tree& tree, int n) {
   // напишите реализацию
+    std::vector<std::vector<char>> recs = tree.receive(3);
+    if (recs.size() < n)
+        return {};
+    return recs[n - 1];
 }
