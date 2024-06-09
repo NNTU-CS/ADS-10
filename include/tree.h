c993e73aef2ced33be7bbdf7167530cf2fb01ae9
VladRomashkin <150885042+VlRom@users.noreply.github.com>
2024-06-09 23:47:58 +0300

    Update tree.h

--- include/tree.h
+++ include/tree.h
@@ -2,4 +2,26 @@
 #ifndef INCLUDE_TREE_H_
 #define INCLUDE_TREE_H_
 
+#include <vector>
+#include <memory>
+
+class TreeNode {
+ public:
+    explicit TreeNode(char val) : value(val) {}
+    char value;
+    std::vector<std::shared_ptr<TreeNode>> children;
+};
+
+class Tree {
+ public:
+    explicit Tree(const std::vector<char>& elements);
+    std::shared_ptr<TreeNode> getRoot() const { return root; }
+    void buildTree(const std::vector<char>& elements);
+
+ private:
+    std::shared_ptr<TreeNode> root;
+    void buildTreeRecursive(std::shared_ptr<TreeNode> node,
+                            const std::vector<char>& elements);
+};
+
 #endif  // INCLUDE_TREE_H_
