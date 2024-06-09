15657ecc263a015f373f7881a0efc1ef5a8cd2b1
PolinaGribkova <160274174+PolinaGribkova@users.noreply.github.com>
2024-06-09 16:04:04 +0300

    Update tree.h

--- include/tree.h
+++ include/tree.h
@@ -10,6 +10,7 @@ struct Node {
   char val;
   std::vector<Node*> permutations;
 };
+
 class Tree {
  private:
   Node* mm_root;
@@ -20,19 +21,19 @@ class Tree {
       temp->val = c;
       root->permutations.push_back(temp);
       std::vector<char> remainingChars(vect_perm);
-      remainingChars.erase(std::find(remainingChars.begin(), 
+      remainingChars.erase(std::find(remainingChars.begin(),
       remainingChars.erase(std::find(remainingChars.begin(),
         remainingChars.end(), c));
       append(temp, remainingChars);
     }
   }
   void findP(Node* root, std::vector<char> vect_perm) {
-    if (!root->root) { 
-	    vect_perm.push_back(root->val);
-	}
+    if (!root->root) {
+        vect_perm.push_back(root->val);
+    }
     if (root->permutations.empty()) {
         permut.push_back(vect_perm);
-	}
+    }
     for (auto child : root->permutations) {
         findP(child, vect_perm);
     }
@@ -48,4 +49,5 @@ class Tree {
   std::vector<std::vector<char> > getPermutations() const {
     return permut;
   }
+};
 #endif  // INCLUDE_TREE_H_
