f703f98ebdddb79f2d488e11aa191e8bed846c31
Ksenia-Kvashnina <151324796+Ksenia-Kvashnina@users.noreply.github.com>
2024-06-09 22:10:31 +0300

    Update tree.h

--- include/tree.h
+++ include/tree.h
@@ -1,5 +1,19 @@
 // Copyright 2022 NNTU-CS
 #ifndef INCLUDE_TREE_H_
 #define INCLUDE_TREE_H_
+#include <string>
+#include <vector>
+#include <cstdint>
 
+class Tree {
+ private:
+    struct Node;
+    std::vector<std::string> perms;
+    Node* root;
+    void buildTree(Node* root, std::vector<char> trl);
+    void gen_perms(Node* root, std::string symb);
+ public:
+    explicit Tree(const std::vector<char>);
+    std::string operator[] (unsigned int i) const;
+};
 #endif  // INCLUDE_TREE_H_
