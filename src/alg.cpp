258a3deaec19c92d614d5264f32e70d9ca154919
MoklokovaKs <160238048+MoklokovaKs@users.noreply.github.com>
2024-06-09 18:10:21 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -5,40 +5,17 @@
 #include  <cstdlib>
 #include  "tree.h"
 
-Tree::Tree(const std::vector<char>& in) {
-  buildTree(in);
+std::vector<char> Delete(std::vector<char> numbers, int index) {
+    if (index < numbers.size() && index >= 0) {
+        numbers.erase(numbers.begin() + index);
+    }
+    return numbers;
 }
 
-void Tree::buildTree(const std::vector<char>& in) {
-  if (in.empty()) {
-    return;
-  }
-  std::vector<char> left(in.begin() + 1, in.end());
-  std::vector<char> right(in.begin() + 1, in.end());
-  buildTree(left);
-  buildTree(right);
-}
 
 std::vector<char> getPerm(const Tree& tree, int n) {
-  if (n < 1) {
-    return {};
-  }
-  return tree.getPermRecursive(n, 0);
-}
-
-std::vector<char> Tree::getPermRecursive(int n, int level) {
-  if (n > 1) {
-    std::vector<char> left = getPermRecursive(n - 1, level + 1);
-    std::vector<char> right = getPermRecursive(n - 1, level + 1);
-    std::vector<char> result;
-    for (char c : left) {
-      for (char d : right) {
-        result.push_back(c);
-        result.push_back(d);
-      }
-    }
-    return result;
-  } else {
-    return {};
-  }
+    std::vector<std::vector<char>> perm = tree.getPerms(3);
+    if (perm.size() < n)
+        return {};
+    return perm[n - 1];
 }
