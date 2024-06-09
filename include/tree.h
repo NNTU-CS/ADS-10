f1861388f054b495707edb0d2fdfb13b7f826662
shorokhovkir <160240169+shorokhovkir@users.noreply.github.com>
2024-06-09 21:54:15 +0300

    Update tree.h

--- include/tree.h
+++ include/tree.h
@@ -3,18 +3,18 @@
 #define INCLUDE_TREE_H_
 
 #include <string>
-#incldue <vector>
+#include <vector>
 #include <cstdint>
 
 class Tree {
  private:
-   struct Node;
-   std::vector<std::string> perms;
-   Node* root;
-   void buildTree(Node* root, std::vector<char> trl);
-   void gen_perms(Node* root, std::string symb);
+    struct Node;
+    std::vector<std::string> perms;
+    Node* root;
+    void buildTree(Node* root, std::vector<char> trl);
+    void gen_perms(Node* root, std::string symb);
  public:
-   explicit Tree(const std::vector<char>);
-   std::string opertr[] (unsigned int i) const;
+    explicit Tree(const std::vector<char>);
+    std::string opertr[] (unsigned int i) const;
 };
 #endif  // INCLUDE_TREE_H_
