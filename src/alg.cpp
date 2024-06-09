0be2f249c39982247e25514af536ed99d9fe5b0f
Ilona <150517376+LunyaLp@users.noreply.github.com>
2024-06-09 14:24:24 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -5,6 +5,33 @@
 #include  <cstdlib>
 #include  "tree.h"
 
-std::vector<char> getPerm(const Tree& tree, int n) {
-  // напишите реализацию
+int factor(int i) {
+  int result = 1;
+  for (int j = 1; j <= i; j++) {
+    result *= j;
+  }
+  return result;
+}
+
+std::vector<char> getPerm(const Tree &tree, int number) {
+  std::vector<char> result;
+  if (number <= factor(tree.rot->nodes.size())) {
+    Node *rot = tree.rot;
+    int i = factor(rot->nodes.size()) / rot->nodes.size();
+    for (int j = 0; j < rot->nodes.size(); j++) {
+      if ((number <= i * (j + 1)) && (number > j * 2)) {
+        rot = rot->nodes[j];
+        result.push_back(rot->val);
+      }
+    }
+    if (number % 2 == 0) {
+      rot = rot->nodes[1];
+    } else {
+      rot = rot->nodes[0];
+    }
+    result.push_back(rot->val);
+    rot = rot->nodes[0];
+    result.push_back(rot->val);
+  }
+  return result;
 }
