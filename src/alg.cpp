a83d571b3790b25af3cfd928563f7641798fa82d
TailorOfEnbizaka <160338576+TailorOfEnbizaka@users.noreply.github.com>
2024-06-09 12:12:26 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -5,6 +5,17 @@
 #include  <cstdlib>
 #include  "tree.h"
 
+int F(int n) {
+  if (n == 0)
+    return 1;
+  return n * F(n - 1);
+}
 std::vector<char> getPerm(const Tree& tree, int n) {
   // напишите реализацию
+  std::vector<char> res = {};
+  if (n < 0 || F(tree.Size()) < n)
+    return{};
+  res = tree.Got(n - 1);
+  res.erase(res.begin());
+  return res;
 }
