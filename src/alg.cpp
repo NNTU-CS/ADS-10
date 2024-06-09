ccc2bdf706763fc5b9f69d18c490fa76447da714
VladRomashkin <150885042+VlRom@users.noreply.github.com>
2024-06-09 23:54:56 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,10 +1,10 @@
 // Copyright 2022 NNTU-CS
-#include  <iostream>
-#include  <fstream>
-#include  <locale>
-#include  <cstdlib>
-#include  "tree.h"
+#include <iostream>
+#include <fstream>
+#include <locale>
+#include <cstdlib>
 #include <algorithm>
+#include "tree.h"
 
 void generatePermutations(std::shared_ptr<TreeNode> node,
                           std::vector<char>* current_permutation,
