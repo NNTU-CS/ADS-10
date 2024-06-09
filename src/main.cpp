2d38f2ea62d682d566a1b41b1f629e953d8e651c
BaevGeorgy55 <160393440+BaevGeorgy55@users.noreply.github.com>
2024-06-09 20:21:09 +0300

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,19 +1,19 @@
 // Copyright 2022 NNTU-CS
 #include "tree.h"
 
-std::vector<char> getPerm(const Tree& tree, int n);
+std::vector<char> getPerm(const Drevo& drevo, int n);
 
 int main() {
   // определяем входной вектор для дерева
   std::vector<char> in = {'1', '2', '3'};
 
   // создаем дерево
-  Tree tree(in);
+  Drevo drevo(in);
 
   // запрашиваем первую перестановку
-  std::vector<char> result1 = getPerm(tree, 1);  //  123
+  std::vector<char> result1 = getPerm(drevo, 1);  //  123
 
   // запрашиваем вторую перестановку
-  std::vector<char> result2 = getPerm(tree, 2);  //  132
+  std::vector<char> result2 = getPerm(drevo, 2);  //  132
   return 0;
 }
