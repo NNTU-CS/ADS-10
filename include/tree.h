2d71c051117f07bd48e7d4db3d14f96a0056cbc2
MoklokovaKs <160238048+MoklokovaKs@users.noreply.github.com>
2024-06-09 17:19:56 +0300

    Update tree.h

--- include/tree.h
+++ include/tree.h
@@ -1,5 +1,16 @@
 // Copyright 2022 NNTU-CS
 #ifndef INCLUDE_TREE_H_
 #define INCLUDE_TREE_H_
+#include <vector>
+#include <string>
+
+class Tree {
+public:
+    Tree(const std::vector<char>& in);
+    std::vector<char> getPerm(int n);
+private:
+    void buildTree(const std::vector<char>& in);
+    std::vector<char> getPermRecursive(int n, int level);
+};
 
 #endif  // INCLUDE_TREE_H_
