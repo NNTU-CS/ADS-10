376a336e02e570f67acfeae94f2fa1b58517001c
MoklokovaKs <160238048+MoklokovaKs@users.noreply.github.com>
2024-06-09 17:39:29 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -5,6 +5,40 @@
 #include  <cstdlib>
 #include  "tree.h"
 
-std::vector<char> getPerm(const Tree& tree, int n) {
-  // напишите реализацию
+Tree::Tree(const std::vector<char>& in) {
+  buildTree(in);
+}
+
+void Tree::buildTree(const std::vector<char>& in) {
+  if (in.empty()) {
+    return;
+  }
+  std::vector<char> left(in.begin() + 1, in.end());
+  std::vector<char> right(in.begin() + 1, in.end());
+  buildTree(left);
+  buildTree(right);
+}
+
+std::vector<char> Tree::getPerm(int n) {
+  if (n < 1) {
+    return {};
+  }
+  return getPermRecursive(n, 0);
+}
+
+std::vector<char> Tree::getPermRecursive(int n, int level) {
+  if (n > 1) {
+    std::vector<char> left = getPermRecursive(n - 1, level + 1);
+    std::vector<char> right = getPermRecursive(n - 1, level + 1);
+    std::vector<char> result;
+    for (char c : left) {
+      for (char d : right) {
+        result.push_back(c);
+        result.push_back(d);
+      }
+    }
+    return result;
+  } else {
+    return {};
+  }
 }
